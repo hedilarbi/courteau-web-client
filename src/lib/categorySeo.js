ccc03dc552749c69fd8f30d8ea1c09e72b74f92f
hedi larbi <hedilarbi@hedis-MacBook-Air.local>
2026-09-12 14:04:47 +0100

    12

--- src/lib/categorySeo.js
+++ src/lib/categorySeo.js
@@ -7,4 +7,12 @@ export const categorySeo = {
     intro:
       "Découvrez les pizzas du Casse-Croûte Courteau à Trois-Rivières et parcourez les choix disponibles au menu.",
   },
+  "le-poulet-frit-courteau": {
+    title: "Poulet frit à Trois-Rivières | Casse-Croûte Courteau",
+    description:
+      "Découvrez notre poulet frit à Trois-Rivières au Casse-Croûte Courteau. Consultez les choix disponibles au menu et commandez en ligne.",
+    h1: "Poulet frit à Trois-Rivières",
+    intro:
+      "Découvrez le poulet frit du Casse-Croûte Courteau à Trois-Rivières et parcourez les choix disponibles au menu.",
+  },
 };
