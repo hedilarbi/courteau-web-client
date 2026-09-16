832b632d4607f8f8be13d8b39414a445e8165ad7
hedi larbi <hedilarbi@hedis-MacBook-Air.local>
2026-09-16 13:56:38 +0100

    12

--- src/app/(site)/menu/cadeaux/page.js
+++ src/app/(site)/menu/cadeaux/page.js
@@ -1,5 +1,12 @@
 import SmartOfferGiftSelection from "@/components/SmartOfferGiftSelection";
 
+export const metadata = {
+  robots: {
+    index: false,
+    follow: true,
+  },
+};
+
 export default function SmartOfferGiftsPage() {
   return <SmartOfferGiftSelection />;
 }
